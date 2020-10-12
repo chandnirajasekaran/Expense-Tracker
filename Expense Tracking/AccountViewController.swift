412083c15e7b49ed2ccbd61a4222104bfd5e35dd
chandnirajasekaran <56169756+chandnirajasekaran@users.noreply.github.com>
2020-10-11 22:43:03 -0700

    Update AccountViewController.swift
    version 2

--- Expense Tracking/AccountViewController.swift
+++ Expense Tracking/AccountViewController.swift
@@ -13,26 +13,31 @@ import Firebase
 class AccountViewController: UIViewController {
     
     @IBOutlet var accountEmail: UILabel!
+    var emailAccount = ""
 
     override func viewDidLoad() {
         view.setGradientBackground(colorOne: UIColor(red:70.0/255.0, green: 67.0/255.0, blue:66.0/255.0, alpha:1.0), colorTwo: UIColor(red:109.0/255.0, green: 107.0/255.0, blue:107.0/255.0, alpha:1.0), colorThree: UIColor(red:70.0/255.0, green: 67.0/255.0, blue:66.0/255.0, alpha:1.0))
-        if(Auth.auth().currentUser!.email != nil) {
+        if(Auth.auth().currentUser?.email == nil) {
+            accountEmail.text = "Google Account"
+        } else {
             accountEmail.text = Auth.auth().currentUser!.email!
+        
         }
         super.viewDidLoad()
     }
 
     @IBAction func logOutTapped(_ sender: Any) {
-        if(Auth.auth().currentUser!.email == nil) {
-            print(Auth.auth().currentUser!.email!)
-        }
+        if(Auth.auth().currentUser?.email == nil) {
+            GIDSignIn.sharedInstance()?.signOut()
+        } else {
+        print("INSIDE HEHEHEH")
         let firebaseAuth = Auth.auth()
         do {
             try firebaseAuth.signOut()
         } catch let signOutError as NSError {
          print ("Error signing out: %@", signOutError)
         }
-        GIDSignIn.sharedInstance()?.signOut()
+        }
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(identifier: "login")
         vc.modalPresentationStyle = .overFullScreen
@@ -41,5 +46,3 @@ class AccountViewController: UIViewController {
     
 
 }
-
-
