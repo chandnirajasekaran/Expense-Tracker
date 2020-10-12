af88bf6fe305d0560ba7589bab949a81905102ab
chandnirajasekaran <56169756+chandnirajasekaran@users.noreply.github.com>
2020-10-11 22:48:25 -0700

    Update EditViewController.swift
    version 2

--- Expense Tracking/EditViewController.swift
+++ Expense Tracking/EditViewController.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2020 Chandni Rajasekaran. All rights reserved.
 //
 
-import RealmSwift
 import UIKit
+import Firebase
 
 class EditViewController: UIViewController {
 
@@ -18,22 +18,22 @@ class EditViewController: UIViewController {
     @IBOutlet var  itemLabel: UILabel!
     @IBOutlet var  priceLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
-    
-    private let realm = try! Realm()
+    var db =  Firestore.firestore()
     
     static let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = .medium
         return dateFormatter
     }()
+    var itemId = "temporary"
     
     override func viewDidLoad() {
         view.setGradientBackground(colorOne: UIColor(red:70.0/255.0, green: 67.0/255.0, blue:66.0/255.0, alpha:1.0), colorTwo: UIColor(red:109.0/255.0, green: 107.0/255.0, blue:107.0/255.0, alpha:1.0), colorThree: UIColor(red:70.0/255.0, green: 67.0/255.0, blue:66.0/255.0, alpha:1.0))
         super.viewDidLoad()
         itemLabel.text = item!.item
         priceLabel.text = "$" + item!.price
-        dateLabel.text = Self.dateFormatter.string(from: item!.date)
-
+        dateLabel.text = item!.date
+        itemId = item!.docId
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteTapped))
         // Do any additional setup after loading the view.
     }
@@ -43,11 +43,28 @@ class EditViewController: UIViewController {
         guard let currentItem = self.item else{
             return
         }
-        realm.beginWrite()
-        realm.delete(currentItem)
-        try! realm.commitWrite()
-        deletionHandler?()
-        navigationController?.popToRootViewController(animated: true)
+        if(Auth.auth().currentUser?.email == nil) {
+            db.collection("googleUser").document(itemId).delete() { err in
+                if let err = err {
+                    print("Error removing document: \(err)")
+                } else {
+                    print("Document successfully removed!")
+                }
+            }
+            
+        } else {
+            db.collection(Auth.auth().currentUser!.email!).document(itemId).delete() { err in
+                if let err = err {
+                    print("Error removing document: \(err)")
+                } else {
+                    print("Document successfully removed!")
+                }
+            }
+        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "tab")
+        vc?.modalPresentationStyle = .overFullScreen
+        self.present(vc!, animated: true)
     }
 
 }
