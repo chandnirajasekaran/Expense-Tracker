b36c0421bb1fa0c80bd57a917a93f185bc5c0709
chandnirajasekaran <56169756+chandnirajasekaran@users.noreply.github.com>
2020-10-11 22:48:57 -0700

    Update FormViewController.swift
    version 2

--- Expense Tracking/FormViewController.swift
+++ Expense Tracking/FormViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Chandni Rajasekaran. All rights reserved.
 //
 
-import RealmSwift
+import Firebase
 import UIKit
 
 class FormViewController: UIViewController, UITextFieldDelegate {
@@ -15,12 +15,14 @@ class FormViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var priceField: UITextField!
     @IBOutlet var datePicker: UIDatePicker!
     
-    private let realm = try! Realm()
-    public var completionHandler: (() -> Void)?
     
+    public var completionHandler: (() -> Void)?
+    var db =  Firestore.firestore()
+  
     override func viewDidLoad() {
         view.setGradientBackground(colorOne: UIColor(red:70.0/255.0, green: 67.0/255.0, blue:66.0/255.0, alpha:1.0), colorTwo: UIColor(red:109.0/255.0, green: 107.0/255.0, blue:107.0/255.0, alpha:1.0), colorThree: UIColor(red:70.0/255.0, green: 67.0/255.0, blue:66.0/255.0, alpha:1.0))
         super.viewDidLoad()
+
         textField.becomeFirstResponder()
         textField.delegate = self
         priceField.becomeFirstResponder()
@@ -33,9 +35,7 @@ class FormViewController: UIViewController, UITextFieldDelegate {
                 print(selectedDate)
         datePicker.setDate(Date(), animated: true)
         
-        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(saveButtonTapped))
-        // Do any additional setup after loading the view.
     }
     
     func priceFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -49,16 +49,23 @@ class FormViewController: UIViewController, UITextFieldDelegate {
     
     @objc func saveButtonTapped() {
         if let text =  textField.text, !text.isEmpty{
-            let date = datePicker.date
+            let formatter1 = DateFormatter()
+            formatter1.dateStyle = .medium
+            let date = formatter1.string(from: datePicker.date)
             let price = priceField.text
-            realm.beginWrite()
             let newItem = PurchaseItems()
             newItem.date = date
             newItem.price = price ?? "0.00"
-            newItem.item = text
-            realm.add(newItem)
-            try! realm.commitWrite()
-            
+            if(Auth.auth().currentUser?.email == nil) {
+                print("setting")
+                let newDocument = db.collection("googleUser").document()
+                newDocument.setData(["text":text, "price": price!, "id": newDocument.documentID, "date": date])
+                newItem.docId = newDocument.documentID
+            } else {
+                let newDocument = db.collection(Auth.auth().currentUser!.email!).document()
+                newDocument.setData(["text":text, "price": price!, "id": newDocument.documentID, "date": date])
+                newItem.docId = newDocument.documentID
+            }
             completionHandler?()
             navigationController?.popToRootViewController(animated: true)
         } else {
